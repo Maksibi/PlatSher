7d6e5c122be8c0c3be9564407e0db8469356329f
Maksibi <Maks.bog@list.ru>
2023-07-13 15:16:36 +0700

    commit
    statemachine update

--- Assets/Scripts/Entities/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Entities/Player/PlayerStateMachine.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Player
+public class PlayerStateMachine 
 {
-    public class PlayerStateMachine
+    public PlayerState currentState { get; private set; }
+
+    public void Initialize(PlayerState _startState)
     {
-        public PlayerState currentState { get; private set; }
+        currentState = _startState;
+        currentState.Enter();
+    }
 
-        public void Initialize(PlayerState _startState)
-        {
-            currentState = _startState;
-            currentState.Enter();
-        }
-
-        public void ChangeState(PlayerState _newState)
-        {
-            currentState.Exit();
-            currentState = _newState;
-            currentState.Enter();
-        }
+    public void ChangeState(PlayerState _newState)
+    {
+        currentState.Exit();
+        currentState = _newState;
+        currentState.Enter();
     }
 }
